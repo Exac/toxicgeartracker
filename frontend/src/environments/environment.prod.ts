d89d8d5531d852e72f8f1ae118db2051cc0c236b
Thomas <tgmclennan@hotmail.com>
2020-08-11 00:47:30 -0700

    feat: add backend config to frontend environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -3,5 +3,9 @@ export const environment = {
   wclApiKey: '688e15caae84a659990c486a78fc6383',
   wclAccount: 'pronator',
   guild: 'toxic',
-  server: 'fairbanks'
+  server: 'fairbanks',
+  backend: {
+    uri: 'http://localhost',
+    port: '3000',
+  },
 };
