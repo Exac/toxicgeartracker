18f09bb25c9ef1dcb810e344b02c7f03cb16ffbd
Thomas <tgmclennan@hotmail.com>
2020-08-13 01:51:41 -0700

    feat: change shared module exports

--- toxicgeartracker-shared/src/index.ts
+++ toxicgeartracker-shared/src/index.ts
@@ -1,1 +1,4 @@
-export { Item, Player, Equips, IFetchPlayerGearDto } from "models/index";
+export { Item } from "models/item";
+export { Player } from "models/player";
+export { Equips } from "models/equips";
+export { IFetchPlayerGearDto } from "models/fetch-player-gear.dto.interface";
